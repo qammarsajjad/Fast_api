a29de24536a26d6eca02432cebe062b7a251b325
Qammar <qammarsahu@gmail.com>
2023-03-16 06:25:08 +0500

    Udated

--- blog/database.py
+++ blog/database.py
@@ -1,6 +1,16 @@
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-SQLALCHAMY_DATABASE_URL ='sqlite:///./blog.db'
 
 
-engine = create_engine(SQLALCHAMY_DATABASE_URL,connect_args={"check_same_thread": False})
+
+SQLALCHEMY_DATABASE_URL ='sqlite:///./blog.db'
+
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args={"check_same_thread": False})
+
+
+Session = sessionmaker(bind=engine, autocommit=False, autoflush=False,)
+
+Base = declarative_base()
--- blog/main.py
+++ blog/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
-from . import schemas
+from . import schemas,models
+from  .database import engine
 
 
 
 
 app = FastAPI()
 
-
+models.Base.metadata.create_all(bind=engine)
 
 
 @app.post('/blog')
