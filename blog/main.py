b8fbb89c04a7d29c4dd69995f667d9489f70be8f
Qammar <qammarsahu@gmail.com>
2023-03-21 02:37:36 +0500

    Updated

--- blog/main.py
+++ blog/main.py
@@ -27,7 +27,7 @@ def get_db():
 
  
 
-@app.post('/blog', status_code=status.HTTP_201_CREATED)
+@app.post('/blog', status_code=status.HTTP_201_CREATED,tags=['blogs'])
 def create(request:schemas.Blog,db :Session =Depends(get_db)):
     new_blog = models.Blog(hotel_name=request.hotel_name,address=request.address,multi_type_hotel=request.multi_type_hotel,
     star_rating=request.star_rating,currency=request.currency,property_description=request.property_description,property_highlights=request.property_highlights 
@@ -37,7 +37,7 @@ def create(request:schemas.Blog,db :Session =Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
-@app.post('/contact', status_code=status.HTTP_201_CREATED)
+@app.post('/contact', status_code=status.HTTP_201_CREATED,tags=['contacts'])
 def create_contact(request:schemas.Contact,db :Session =Depends(get_db)):
     new_contact= models.Contact(owner_nmb=request.owner_nmb, manger_nmb=request.manger_nmb,front_office_nmbr=request.front_office_nmbr)
     db.add(new_contact)
@@ -45,7 +45,7 @@ def create_contact(request:schemas.Contact,db :Session =Depends(get_db)):
     db.refresh(new_contact)
     return new_contact
 
-@app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT)
+@app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT,tags=['blogs'])
 def delete(id,db :Session =Depends(get_db)):
         blog = db.query(models.Blog).filter(models.Blog.id == id)
 
@@ -58,7 +58,7 @@ def delete(id,db :Session =Depends(get_db)):
         db.commit()
         return 'Done'
 
-@app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED)
+@app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED,tags=['blogs'])
 def update(id,request:schemas.Blog,db :Session =Depends(get_db)):
      blog = db.query(models.Blog).filter(models.Blog.id == id)
 
@@ -72,20 +72,20 @@ def update(id,request:schemas.Blog,db :Session =Depends(get_db)):
      return f'Sucessfuly Updated!!!'
            
 
-@app.get('/blog',response_model=List[schemas.ShowBlog])
+@app.get('/blog',response_model=List[schemas.ShowBlog],tags=['blogs'])
 def all(db :Session =Depends(get_db)):
 
     blogs = db.query(models.Blog).all()
 
     return blogs
 
-@app.get('/contact')
+@app.get('/contact',tags=['contacts'])
 def all(db :Session =Depends(get_db)):
     contacts = db.query(models.Contact).all()
     return contacts
 
 
-@app.get('/blog/{id}',status_code=200,response_model=schemas.ShowBlog)
+@app.get('/blog/{id}',status_code=200,response_model=schemas.ShowBlog,tags=['blogs'])
 def show(id, response:Response ,db :Session =Depends(get_db)):
 
     blog = db.query(models.Blog).filter(models.Blog.id==id).first()
@@ -95,9 +95,10 @@ def show(id, response:Response ,db :Session =Depends(get_db)):
 
     return blog
 
+
 pwd_cxt = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-@app.post('/user/')
+@app.post('/user/',response_model=schemas.ShowUser,tags=['users'])
 def create_user(request: schemas.User,db :Session =Depends(get_db)):
     hashedPassword = pwd_cxt.hash(request.password)
     new_user = models.User( name=request.name, email=request.email, password= hashedPassword )
@@ -106,5 +107,11 @@ def create_user(request: schemas.User,db :Session =Depends(get_db)):
     db.refresh(new_user)
     return new_user
 
-  
+
+@app.get('/user/{id}',response_model=schemas.ShowUser,tags=['users'])
+def get_user(id:int,db :Session =Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+          raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"The User wit id  {id} is not available")
     
+    return user
--- blog/schemas.py
+++ blog/schemas.py
@@ -31,3 +31,12 @@ class User(BaseModel):
     name:str
     email:str
     password:str
+
+class ShowUser(BaseModel):
+    name:str
+    email:str
+
+    class Config:
+       orm_mode = True
+
+  
